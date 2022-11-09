e912fd7c5202c9ff3d3049368d576a675070313f
Gyujin Song <songgo0672@naver.com>
2022-11-09 11:37:56 +0900

    첫번째 커밋

---
+++ src/Helloworld.java
@@ -0,0 +1,21 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 주석 : 한 라인 주석 입니다.
+			// 주석은 프로그램에서 실행되지 않는 부분
+		/*
+		 * 여러 라인 주석 입니다.
+		 */
+		
+		System.out.println("권도혁 ");
+		
+		System.out.println("3cm");
+		
+		System.out.println("끼룩.");
+		    //syso + <ctrl + spacebar>
+		
+	}
+
+}
---
+++ src/packeag.java
@@ -0,0 +1,12 @@
+
+public class packeag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//	기본클래스명은 반드시 폿주나느 영어 대문자로지정
+		//  - 동일한 패키지 내에서 ㄷ름으 덜이시
+		
+
+	}
+
+}
